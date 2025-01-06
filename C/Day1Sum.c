f9c98ad42d8406b8370239d33c98504237427cb5
ArpitRai30 <arpitrai2004@outlook.com>
2025-01-07 00:34:29 +0530

    Initial commit

---
+++ C/Day1Sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int a=10, b=20, c=30, sum=0, avg=0;
+    int sum=a+b+c;
+    int avg=sum/3;
+    printf("The average of three numbers is %d", avg);
+    return 0;
+
+}
