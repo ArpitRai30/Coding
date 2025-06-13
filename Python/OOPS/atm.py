dbf4031fbe13682b524099f8a8acdf31725b1b1c
ArpitRai30 <arpitrai2004@outlook.com>
2025-06-14 00:04:44 +0530

    day2

--- Python/OOPS/atm.py
+++ Python/OOPS/atm.py
@@ -3,21 +3,37 @@ class acc():
     def __init__(self):
         self.bal = 0
         print("You are successfully registered")
-    def depo(self):
-        self.dep = int(input("Enter deposit amount"))
-        self.bal += self.dep
+    def depo(self, depo):
+        self.bal += depo
+        print("Amount {} is credited to your account".format(depo))
         print("Your account balance is", self.bal)
-    def withd(self):
-        self.wit = int(input("Enter withdrawl amount"))
-        if self.wit <= self.bal:
-            self.bal -= self.wit
+    def wit(self, wit):
+        if 0 < wit <= self.bal:
+            self.bal -= wit
+            print("Amount {} is debited from your account".format(wit))
             print("Your account balance is", self.bal)
         else:
             print("Insufficient balance")
     def check(self):
-        print(self.bal)
+        print("Your account balance is",self.bal)
 
 p = acc()
-p.depo()
-p.withd()
-p.check()
+while True:
+    print("Choose operation from menu")
+    print("1. Check balance")
+    print("2. Deposit amount")
+    print("3. Withdraw amount")
+    print("4. Exit")
+    choice = int(input("Enter your choice:"))
+    if choice == 1:
+        p.check()
+    elif choice == 2:
+        d = int(input("Enter deposit amount:"))
+        p.depo(d)
+    elif choice == 3:
+        w = int(input("Enter withdrawl amount:"))
+        p.wit(w)
+    elif choice == 4:
+        break
+    else:
+        print("Invalid input")
